a9aa3925733879c8acf539f60b40d9ce78daf679
Shano1993 <stefan.hanotiau@gmail.com>
2022-02-25 14:11:39 +0100

    php

--- index.php
+++ index.php
@@ -10,3 +10,66 @@
  *
  * ( PS: Sélectionnez, mais affichez le résultat à chaque fois ! ).
  */
+
+try {
+    $server = 'localhost';
+    $db = 'live';
+    $user = 'root';
+    $password = '';
+
+    $pdo = new PDO("mysql:host=$server;dbname=$db", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+    // Tous les utilisateurs
+    $request = $pdo->prepare("
+        SELECT * FROM user 
+            WHERE EXISTS (SELECT * FROM user)
+    ");
+
+    if ($request->execute()) {
+        foreach ($request->fetchAll() as $value) {
+            echo "Nom : " . $value['username'] . "<br>";
+        }
+    }
+
+    // Tous les articles
+    $request = $pdo->prepare("
+        SELECT * FROM article
+            WHERE EXISTS (SELECT * FROM article)
+    ");
+
+    if ($request->execute()) {
+        foreach ($request->fetchAll() as $value) {
+            echo "Titre : " . $value['titre'] . "<br>" . $value['contenu'] . "<br>";
+        }
+    }
+
+    // Tous les utilisateurs qui parlent de poterie
+    $request = $pdo->prepare("
+        SELECT username FROM user
+            WHERE id = ANY (SELECT user_fk FROM article WHERE contenu LIKE '%poterie%')
+    ");
+
+    if ($request->execute()) {
+        echo "<pre>";
+        print_r($request->fetchAll());
+        echo "</pre>";
+    }
+
+    // Article par Jane
+    $request = $pdo->prepare("
+        SELECT contenu FROM article
+            WHERE user_fk = 6 OR user_fk = 7
+    ");
+
+    if ($request->execute()) {
+        echo "<pre>";
+        print_r($request->fetchAll());
+        echo "</pre>";
+    }
+}
+
+catch (Exception $exception) {
+    echo $exception->getMessage();
+}
